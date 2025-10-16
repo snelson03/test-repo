886816f72f05e4f5ce0992b7ca7866ec2b60b774
Meredith Seta <meredithseta@gmail.com>
2025-10-15 21:13:49 -0400

    Set up base folder structure and starter files

---
+++ study-room-mobile/src/navigation/AppNavigator.tsx
@@ -0,0 +1,19 @@
+// src/navigation/AppNavigator.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import RoomDetailScreen from '../screens/RoomDetailScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="RoomDetail" component={RoomDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
