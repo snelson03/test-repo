c4bfba42e7056a1d7408bcc4103e8cb97d91d392
Seth Nelson <sn280121@ohio.edu>
2025-10-21 20:14:59 -0400

    Finished home/dashboard page

--- study-room-web/src/pages/Home/Home.tsx
+++ study-room-web/src/pages/Home/Home.tsx
@@ -8,24 +8,37 @@ const Home: React.FC = () => {
     <div className="home-container">
       <Sidebar />
       <main className="home-content">
-        <h1>Welcome back, sn280121!</h1>
+        <h1 className="welcome-text">Welcome back, sn280121!</h1>
 
         <section className="summary">
           <div className="summary-bar">
             <div className="summary-item">
               <span className="building">Stocker Center</span>
-              <span className="rm_status">All rooms full</span>
+              <div className="rm_status">
+                <span class="status-dot red"></span>
+                All rooms full
+              </div>
             </div>
 
             <div className="summary-item">
-              <span className="building">ARC</span>
-              <span className="rm_status">2 rooms free</span>
+              <span className="building">Academic & Research Center</span>
+              <div className="rm_status">
+                <span className="status-dot yellow"></span>
+                2 rooms free
+              </div>
             </div>
 
             <div className="summary-item">
               <span className="building">Alden Library</span>
-              <span className="rm_status">5 rooms free</span>
+              <div className="rm_status">
+                <span className="status-dot green"></span>
+                5 rooms free
+              </div>
             </div>
+
+            <button className="see-rooms-btn">
+              See All Rooms →
+            </button>
 
           </div>
         </section>
@@ -34,12 +47,14 @@ const Home: React.FC = () => {
 
           <section className="favorites-rooms">
             <div className="favorites">
-              <div className="favorites-bar">
-                <span>Favorites</span>
-              </div>
-              <ul>
-                <li>Stocker Center 155</li>
-                <li>Stocker Center 152</li>
+              <div className="favorites-bar">Favorites</div>
+              <ul className="favorites-list">
+                <li className="occupied">Academic & Research Center 155</li>
+                <li className="offline">Academic & Research Center 161</li>
+                <li className="available">Alden Library 216</li>
+                <li className="offline">Alden Library 312</li>
+                <li className="available">Stocker Center 155</li>
+                <li className="available">Stocker Center 152</li>
               </ul>
             </div>
           </section>
