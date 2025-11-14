8b530942e5f39776b2393b202790f345992776fa
Meredith Seta <meredithseta@gmail.com>
2025-11-14 15:40:57 -0500

    login screen implemented with mock authentication + logout in preferences

--- study-room-mobile/src/navigation/AppNavigator.tsx
+++ study-room-mobile/src/navigation/AppNavigator.tsx
@@ -8,15 +8,17 @@ import CampusMapScreen from '@/screens/CampusMapScreen';
 import FavoritesScreen from '@/screens/FavoritesScreen';
 import PreferencesScreen from '@/screens/PreferencesScreen';
 import RoomDetailsScreen from '@/screens/RoomDetailsScreen';
+import LoginScreen from '@/screens/LoginScreen';
 
 // setting up app pages
 export type RootStackParamList = {
+  Login: undefined;
   Home: undefined;
   FindRoom: undefined;
   CampusMap: undefined;
   Favorites: undefined;
   Preferences: undefined;
-  RoomDetails: { 
+  RoomDetails: {
     building: 'Stocker Center' | 'ARC' | 'Alden Library';
     roomId: string;
     status: 'available' | 'occupied' | 'offline';
@@ -28,7 +30,12 @@ const Stack = createNativeStackNavigator();
 // setting up navigation routes for pages
 export default function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen 
         name="Home" 
         component={HomeScreen} 
@@ -37,29 +44,28 @@ export default function AppNavigator() {
       <Stack.Screen 
         name="FindRoom" 
         component={FindRoomScreen} 
-        options={{ headerShown: false  }} 
+        options={{ headerShown: false }} 
       />
       <Stack.Screen 
         name="CampusMap" 
         component={CampusMapScreen} 
-        options={{ headerShown: false  }} 
+        options={{ headerShown: false }} 
       />
       <Stack.Screen 
         name="Favorites" 
         component={FavoritesScreen} 
-        options={{ headerShown: false  }} 
+        options={{ headerShown: false }} 
       />
       <Stack.Screen 
         name="Preferences" 
         component={PreferencesScreen} 
-        options={{ headerShown: false  }} 
+        options={{ headerShown: false }} 
       />
       <Stack.Screen
         name="RoomDetails"
         component={RoomDetailsScreen}
         options={{ headerShown: false }}
       />
-
     </Stack.Navigator>
   );
 }
