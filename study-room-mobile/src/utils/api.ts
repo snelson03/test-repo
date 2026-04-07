a25bf3e4d306fd126d6b9646e06a1150119b22b6
Seth Nelson <sn280121@ohio.edu>
2026-04-06 20:21:53 -0400

    update api

--- study-room-mobile/src/utils/api.ts
+++ study-room-mobile/src/utils/api.ts
@@ -7,7 +7,7 @@ const getDefaultApiBaseUrl = () => {
     return "http://10.0.2.2:8000/api/v1";
   }
 
-  return "http://localhost:8000/api/v1";
+  return "https://unconsentaneous-unconscientious-sutton.ngrok-free.dev/api/v1";
 };
 
 const API_BASE_URL =
